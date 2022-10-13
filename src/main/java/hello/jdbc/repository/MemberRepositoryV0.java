b0963b34fce8c3061c894cc0377423788a958aba
문수정 <anstnwjd22@naver.com>
2022-10-14 00:10:55 +0900

    DataSource 적용

--- src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryV0.java
@@ -7,13 +7,11 @@ import lombok.extern.slf4j.Slf4j;
 import java.sql.*;
 import java.util.NoSuchElementException;
 
-import static hello.jdbc.connection.DBConnectionUtil.getConnection;
-
 /**
  * JDBC - DriverManager 사용
  * */
 @Slf4j
-public class MemberRepositoryVO {
+public class MemberRepositoryV0 {
 
     public Member save(Member member) throws SQLException {
         String sql = "insert into member(member_id, money) values (?, ?)"; 
--- src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
+++ src/test/java/hello/jdbc/repository/MemberRepositoryV1Test.java
@@ -1,23 +1,41 @@
 package hello.jdbc.repository;
 
+import com.zaxxer.hikari.HikariDataSource;
 import hello.jdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.NoSuchElementException;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static hello.jdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @Slf4j
-class MemberRepositoryVOTest {
+class MemberRepositoryV1Test {
 
-    MemberRepositoryVO repository = new MemberRepositoryVO();
+    MemberRepositoryV1 repository;
+
+    @BeforeEach
+    void beforeEach(){
+        // 기본 DriverManager - 항상 새로운 커넥션을 획득
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        // 커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        repository = new MemberRepositoryV1(dataSource);
+    }
 
     @Test
-    void crud() throws SQLException {
+    void crud() throws SQLException, InterruptedException {
         // save
         Member member = new Member("memberV100", 10000);
         repository.save(member);
@@ -37,5 +55,8 @@ class MemberRepositoryVOTest {
         // delete
         repository.delete(member.getMemberId());
         assertThatThrownBy(() -> repository.findById(member.getMemberId())).isInstanceOf(NoSuchElementException.class);
+
+        Thread.sleep(1000);
+
     }
 }

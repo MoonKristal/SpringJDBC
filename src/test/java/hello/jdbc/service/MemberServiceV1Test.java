0a743074299595bd010dd5c255dd3ce148657652
문수정 <anstnwjd22@naver.com>
2022-10-16 23:48:15 +0900

    트랜잭션 - 적용1

---
+++ src/test/java/hello/jdbc/service/MemberServiceV1Test.java
@@ -0,0 +1,54 @@
+package hello.jdbc.service;
+
+import hello.jdbc.connection.ConnectionConst;
+import hello.jdbc.repository.MemberRepositoryV1;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import java.lang.reflect.Member;
+import java.sql.Connection;
+
+import static hello.jdbc.connection.ConnectionConst.*;
+
+/**
+ * 기본 동작, 트랜잭션이 없어서 문제 발생
+ * */
+public class MemberServiceV1Test {
+
+    public static final String MEMBER_A = "memberA";
+    public static final String MEMBER_B = "memberB";
+    public static final String MEMBER_EX = "ex";
+
+    private MemberRepositoryV1 memberRepository;
+    private MemberServiceV1 memberService;
+
+    @BeforeEach
+    void before() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+        memberRepository = new MemberRepositoryV1(dataSource);
+        memberService = new MemberServiceV1(memberRepository);
+    }
+
+
+    @Test
+    @DisplayName("정상 이체")
+    void accountTransfer(){
+        // given
+        Member memberA = new Member(MEMBER_A, 10000);
+        Member memberB = new Member(MEMBER_B, 10000);
+        memberRepository.save(memberA);
+        memberRepository.save(memberB);
+
+        // when
+
+
+        // then
+
+
+
+
+    }
+
+}
